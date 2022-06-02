fd2361e4069ed1fec7939da928e827a293de946c
lcmongwe <lcmongwe@gmail.com>
2022-06-02 17:08:49 +0300

    urls.py created

--- instaproject/settings.py
+++ instaproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'instapp',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,10 @@ WSGI_APPLICATION = 'instaproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instapp',
+        'USER': 'lucy',
+    'PASSWORD':'4444',
     }
 }
 
