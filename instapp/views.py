18bbebd9285796440eb041d6612d6723ce7c442b
lcmongwe <lcmongwe@gmail.com>
2022-06-02 17:15:55 +0300

    database configurations set

--- instapp/views.py
+++ instapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+# from .models import Article
+# import datetime as dt
+# from .forms import NewsLetterForm
 
 # Create your views here.
+
+def home(request):
+    
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    return render(request, 'home.html', {})
+
