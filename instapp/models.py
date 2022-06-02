68f64bed72d9bd018e2ce9720baeab734722f003
lcmongwe <lcmongwe@gmail.com>
2022-06-02 23:33:47 +0300

    comment model created

--- instapp/models.py
+++ instapp/models.py
@@ -4,10 +4,21 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Models):
-    Photo
-    name
-    bio
+    Photo=models.ImageField()
+    name=models.CharField(max_length=255)
+    username=models.CharField(max_length=255)
+    bio= models.TextField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    comment= models.TextField()
+    author=models.ForeignKey(Profile, null=True)
+
+
+    def __str__(self):
+            return self.comment
 
 class Image(models.Model):
     name = models.CharField('Venue Name',max_length=120)
