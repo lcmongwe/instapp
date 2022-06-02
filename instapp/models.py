3e1f38e85f59a3dd2e966ba7fad48ca07b8c2002
lcmongwe <lcmongwe@gmail.com>
2022-06-02 23:19:39 +0300

    image model created

--- instapp/models.py
+++ instapp/models.py
@@ -1,3 +1,21 @@
+import profile
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Models):
+    Photo
+    name
+    bio
+
+
+class Image(models.Model):
+    name = models.CharField('Venue Name',max_length=120)
+    image=models.ImageField()
+    captions = models.CharField(max_length=200)
+    profile= models.ForeignKey(Profile)
+    likes= models.IntegerField(default=0)
+    comments= models.TextChoices  
+
+    def __str__(self):
+        return self.name
