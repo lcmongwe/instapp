e527ba03ec9c31532544e3bbb1d980488e75ee67
lcmongwe <lcmongwe@gmail.com>
2022-06-05 17:35:23 +0300

    installed decouple

--- instaproject/settings.py
+++ instaproject/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 # import django_heroku
 # import dj_database_url
-# from decouple import config,Csv
+from decouple import config,Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,3 +138,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# emial configuration settings
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
